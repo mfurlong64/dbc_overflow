5ce38b3c2d03c8144bd1545da7995b1315d3b283
David <email": null>
2016-06-24 15:37:54 -0700

    started ajax for upvote

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,3 +3,10 @@ get '/questions/:id' do
   @answers = @question.answers
   erb :'/questions/show'
 end
+
+get '/questions/:id/vote' do
+  @question = Question.find(params[:id])
+  @question.votes.create(user_id: session[:user_id], value: 1)
+
+  redirect "/questions/#{@question.id}"
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.upvote-button').on('click', function (event) {
+    event.preventDefault();
+    var link = $(this).children().attr('href')
+    console.log(link)
+    $.ajax({
+      url :
+    })
+  })
 });
